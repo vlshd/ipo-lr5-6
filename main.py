af0b1f396c2869c9f7ae355af356caf5398b8182
Valera <valera.savko.2018@mail.ru>
2024-12-12 14:26:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-def a(input_file, output_file): # функция
-    with open(input_file, 'r', encoding='utf-8') as file: # открывает файл
-        text = file.read().lower()  # cчитываем содержимое файла и приводим к нижнему регистру
-        words = text.split()  # разделяем текст на слова
-        words = [word.strip('.,!;()[]') for word in words]  # убираем знаки препинания
-        a_words = set(words)  # находим уникальные слова
+with open("text.txt", "r") as myfile: #открыть файл для чтения
+    text = myfile.read() #чтение текстового файла
+unique = [] #создание пустого списка для уникальных слов
+words = text.split() #разделяем на слова
+for word in words: #проход по каждому слову
+    if word not in unique: #если нет повтор слова
+        unique.append(word) #добавление
 
-    with open(output_file, 'w', encoding='utf-8') as file: # открывает файл
-        for word in sorted(a_words):  # записываем уникальные слова в файл, отсортированные по алфавиту
-            file.write(word + '\n')
-
-# использование функции
-a('text.txt', 'output.txt')
+with open("output.txt", "w") as file: #открывать файл output.txt
+    for word in unique: #разделяем на слова
+        file.write(word + ' ') #записываем
